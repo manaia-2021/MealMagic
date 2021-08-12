789b758ecd6611f0e081e9ca48f52946c30f799c
Brad Booysen <brad.booysen@gmail.com>
2021-08-13 06:48:08 +1200

    Show ingredient list

--- utils.js
+++ utils.js
@@ -2,7 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = {
-  getMealData
+  getMealData,
+  getIngredientsData
 }
 
 function getMealData (callback) {
@@ -17,6 +18,27 @@ function getMealData (callback) {
     try {
       const parsedData = JSON.parse(contents)
       callback(null, parsedData)
+    } catch (parseError) {
+      console.error(parseError)
+      callback(new Error('Unable to parse the data file'))
+    }
+  })
+}
+
+function getIngredientsData (callback) {
+  const filename = path.join(__dirname, 'ingredients.json')
+
+  fs.readFile(filename, 'utf8', (err, contents) => {
+    if (err) {
+      console.error(err.message)
+      callback(new Error('Unable to load the file'))
+      return
+    }
+    try {
+      const parsedData = JSON.parse(contents)
+      callback(null, parsedData)
+      console.log(parsedData)
+      console.log(parsedData)
     } catch (parseError) {
       console.error(parseError)
       callback(new Error('Unable to parse the data file'))
